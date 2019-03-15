a85e07a5af088ed58b341cce9085e7f07c79450f
tmkdev <tekolody@gmail.com>
2019-03-15 15:06:29 -0700

    Arduino lower dotstar brightness. Udev rules for theUSB serial devices. Other small tweaks. 	modified:   carmirror.py 	modified:   circuitpython/main.py 	new file:   udev_rules/50-usb.rules

--- circuitpython/main.py
+++ circuitpython/main.py
@@ -5,6 +5,7 @@ import adafruit_dotstar
 
 pin = analogio.AnalogIn(A0)
 
+brightness=16
 
 pixels = adafruit_dotstar.DotStar(APA102_SCK, APA102_MOSI, 1)
 
@@ -12,15 +13,15 @@ while True:
 	pinvalue = pin.value
 	if pinvalue < 9000:
 		print("BTNS:0")
-		pixels[0] = (128, 128, 0)
+		pixels[0] = (brightness, brightness, 0)
 	elif pinvalue >= 9000 and pinvalue < 20000:
 		print("BTNS:1")
-		pixels[0] = (0, 255, 0)
+		pixels[0] = (0, brightness, 0)
 	elif pinvalue >= 20000 and pinvalue < 40000:
 		print("BTNS:2")
-		pixels[0] = (0, 0, 255)
+		pixels[0] = (0, 0, brightness)
 	elif pinvalue > 40000:
 		print("BTNS:3")
-		pixels[0] = (255, 0, 0)
+		pixels[0] = (brightness, 0, 0)
 
 	time.sleep(0.05)
